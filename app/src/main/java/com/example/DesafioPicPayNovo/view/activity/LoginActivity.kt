c4fe46d233ed31aaaab68f9b785b329950b66236
lubvalentino <lubvalentino@yahoo.com.br>
2021-02-01 13:40:57 -0300

    aula 45

--- app/src/main/java/com/example/DesafioPicPayNovo/view/activity/LoginActivity.kt
+++ app/src/main/java/com/example/DesafioPicPayNovo/view/activity/LoginActivity.kt
@@ -1,123 +1,147 @@
 package com.example.DesafioPicPayNovo.view.activity
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
 import com.example.DesafioPicPayNovo.databinding.ActivityLoginBinding
-import com.facebook.*
-import com.facebook.login.LoginManager
-import com.facebook.login.LoginResult
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-
+import com.facebook.CallbackManager
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private val RC_SIGN_IN = 2
+    private lateinit var binding : ActivityLoginBinding
+
+    private val RC_SIGN_IN = 999
+
+    private val firebaseAuth by lazy {
+        Firebase.auth
+    }
 
     private val callbackManager by lazy {
         CallbackManager.Factory.create()
     }
-//    233954094979-mglag716nqvp8aq8iu8vvo079sbtcf4t.apps.googleusercontent.com
-//    uDqR8970_m4MI4rA1SP8GZ02
 
-    private lateinit var binding: ActivityLoginBinding
-
-    private lateinit var signInClient: GoogleSignInClient
+    private val providers by lazy {
+        arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build(),
+                AuthUI.IdpConfig.FacebookBuilder().build(),
+                AuthUI.IdpConfig.AnonymousBuilder().build()
+        )}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        login google
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
 
-        binding.signInButton.setOnClickListener {
-            val signInIntent: Intent = signInClient.getSignInIntent()
-            startActivityForResult(signInIntent, RC_SIGN_IN)
-        }
+        signUp()
 
-        signInClient = GoogleSignIn.getClient(this, gso)
-
-//       login facebook
-        val accessToken = AccessToken.getCurrentAccessToken()
-        val isLoggedIn = accessToken != null && !accessToken.isExpired
-
-        if (isLoggedIn){
-            binding.loginButton.isVisible = false
-        } else{
-            binding.loginButton.setPermissions("email")
-        }
-
-        LoginManager.getInstance().registerCallback(callbackManager,
-            object : FacebookCallback<LoginResult?> {
-                override fun onSuccess(result: LoginResult?) {
-                    val request = GraphRequest.newMeRequest(
-                        result?.accessToken
-                    ) { jsonResponse, response ->
-                        jsonResponse
-                        response
-                    }
-                    val parameters = Bundle()
-                    parameters.putString("fields", "id,name,link,email")
-                    request.parameters = parameters
-                    request.executeAsync()
-                }
-
-                override fun onCancel() {
-                    // App code
-                }
-
-                override fun onError(error: FacebookException?) {
-                    error
-                }
-            })
-    }
-
-    override fun onResume() {
-        super.onResume()
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-
-        val accessToken = AccessToken.getCurrentAccessToken()
-        val isLoggedIn = accessToken != null && !accessToken.isExpired
-
-        if (isLoggedIn){
-            binding.loginButton.isVisible = false
-        } else{
-            binding.loginButton.setPermissions("email")
-        }
+//        //Login com Facebook
+//        LoginManager.getInstance().registerCallback(callbackManager,
+//            object : FacebookCallback<LoginResult?> {
+//                override fun onSuccess(loginResult: LoginResult?) {
+//                    val token = loginResult?.accessToken
+//                    val facebookCredential = FacebookAuthProvider.getCredential(token?.token ?: "")
+//                    firebaseAuth.signInWithCredential(facebookCredential)
+//                        .addOnSuccessListener {
+//                            binding.tvLoginUserName.text = it.user?.displayName
+//                        }.addOnFailureListener {
+//                            Log.i("teste", it.toString())
+//                            when(it) {
+//                                is FirebaseAuthWeakPasswordException -> {
+//                                    Toast.makeText(this@MainActivity, "Senha fraca", Toast.LENGTH_SHORT).show()
+//                                }
+//                                is FirebaseAuthUserCollisionException -> {
+//                                    Toast.makeText(this@MainActivity, "Esse usuário já existe", Toast.LENGTH_SHORT).show()
+//                                }
+//                                else -> {
+//                                    Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
+//                                }
+//                            }
+//                        }
+//                }
+//
+//                override fun onCancel() {
+//                    Log.i("teste", "teste")
+//                }
+//
+//                override fun onError(exception: FacebookException) {
+//                    exception
+//                }
+//            })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         callbackManager.onActivityResult(requestCode, resultCode, data)
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == RC_SIGN_IN){
-            val task: Task<GoogleSignInAccount> =
-                GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = firebaseAuth.currentUser
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
     }
 
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
+    private fun signUp() {
+        //ação ao clicar no botão - login com email e senha
+        binding.btLoginSignUpEmail.setOnClickListener {
+            //criar o usuário pelo email
+            firebaseAuth.createUserWithEmailAndPassword("teste@teste.com.br", "123456")
+                    //quando for sucesso no login
+                    .addOnSuccessListener {
+                        binding.tvLoginUserName.text = it.user?.displayName
+                        //quando der falha no login
+                    }.addOnFailureListener {
+                        when (it){
+                            //exception senha fraca
+                            is FirebaseAuthWeakPasswordException ->{
+                                Toast.makeText(this, "Senha fraca", Toast.LENGTH_LONG).show()
+                            }
+                            else ->{
+                                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                    }
+        }
 
-            // Signed in successfully, show authenticated UI.
-            binding.signInButton.isVisible = false
-        } catch (e: ApiException) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("TAG", "signInResult:failed code=" + e.statusCode)
+        //ação ao clicar o botão - login anônimo
+        binding.btLoginSignUpAnonymous.setOnClickListener {
+            firebaseAuth.signInAnonymously().addOnSuccessListener {
+                binding.tvLoginUserName.text = "Usuário anônimo"
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //verificar se tem usuário logado
+        firebaseAuth.currentUser?.let {
+            binding.tvLoginUserName.text = it.displayName
+        } ?: run {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN)
         }
     }
 }
